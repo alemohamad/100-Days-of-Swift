42fa620327ac113c9ecc21113f6e7f77081520d6
Ale Mohamad <hello@alemohamad.com>
2019-10-09 09:39:39 +0200

    Day 17 - Project 1, part two

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Ale Mohamad on 09/10/2019.
+//  Copyright © 2019 Ale Mohamad. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+}
--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 [https://www.hackingwithswift.com/100](https://www.hackingwithswift.com/100)
 
 * `Day 16 - Project 1, part one`
- * Setting up
- * Listing images with FileManager
- * Designing our interface
+  * Setting up
+  * Listing images with FileManager
+  * Designing our interface
+* `Day 17 - Project 1, part two`
+  * Building a detail screen
+  * Loading images with UIImage
+  * Final tweaks: hidesBarsOnTap, safe area margins
